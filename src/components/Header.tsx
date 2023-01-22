fac77cf1f35ffd973fa07ad25754e50b892b5f42
CofCat456 <a25690756@gmail.com>
2023-01-23 01:12:38 +0800

    docs: 🐜 add favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,20 @@
+import Link from 'next/link';
+
 import ThemeToggle from './ThemeToggleButton';
 
 import siteMetadata from '@/data/siteMetadata';
 import headerNavLinks from '@/data/headerNavLinks';
-import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
     <header className="sticky top-0 z-50 h-16 w-full overflow-hidden bg-white backdrop-blur transition-colors dark:bg-cc-dark-header-bg">
       <div className="text-light-text dark:text-dark-text mx-auto flex h-full max-w-screen-lg items-center justify-between">
-        <a
-          href="#"
+        <Link
+          href="/"
           className="select-none font-mplus text-2xl font-bold text-cc-text outline-none transition-colors dark:text-cc-dark-text"
         >
           {siteMetadata.headerTitle}
-        </a>
+        </Link>
         <nav className="nav">
           {headerNavLinks.map((link) => (
             <Link key={link.title} href={link.href} className="navItem">

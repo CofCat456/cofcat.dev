df85836ea8fcf66a8591fb962cdb2d2e8ce6b3a5
CofCat456 <a25690756@gmail.com>
2023-01-28 13:53:28 +0800

    feat: 🐈 add antialiased

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import headerNavLinks from '@/data/headerNavLinks';
 
 const Header: React.FC = () => {
   return (
-    <header className="overflw-hidden sticky top-0 z-50 h-16 w-full border-b border-slate-900/10 bg-cc-header backdrop-blur-[10px] transition-colors dark:border-slate-50/[0.06] dark:bg-cc-dark-header-bg">
+    <header className="overflw-hidden sticky top-0 z-50 h-16 w-full border-b border-slate-900/10 bg-cc-header backdrop-blur-[10px] dark:border-slate-50/[0.06] dark:bg-cc-dark-header-bg">
       <div className="mx-auto flex h-full max-w-full items-center justify-between px-4 sm:max-w-screen-sm sm:px-0 md:max-w-screen-md lg:max-w-screen-lg">
         <CustomLink href="/" className="select-none font-mplus text-2xl font-bold outline-none">
           {siteMetadata.headerTitle}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,8 +6,7 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon/favicon.ico" />
       </Head>
-      {/* <body className="bg-cc-bg text-cc-text transition-colors duration-500 dark:bg-cc-dark-bg dark:text-cc-dark-text"> */}
-      <body>
+      <body className="bg-cc-bg text-cc-text antialiased transition-colors duration-500 dark:bg-cc-dark-bg dark:text-cc-dark-text">
         <Main />
         <NextScript />
       </body>

edb5f85d2d08e5847f00031cd0eccc782277bbe0
CofCat456 <a25690756@gmail.com>
2023-02-02 16:36:26 +0800

    docs: 🐜 change favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,12 +6,18 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <script
-                dangerouslySetInnerHTML={{
-                  __html: `${siteMetadata.EasterEgg}`,
-                }}
-              />
+          dangerouslySetInnerHTML={{
+            __html: `${siteMetadata.EasterEgg}`,
+          }}
+        />
       </Head>
       <body className="bg-cc-bg text-cc-text antialiased transition-colors duration-500 dark:bg-cc-dark-bg dark:text-cc-dark-text">
         <Main />
